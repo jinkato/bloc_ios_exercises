c00077140b45eef95121aa3be5232b17f4793708
Jin Kato <jin.kato@starcomww.com>
2015-10-23 14:49:55 -0400

    Assignment 11 completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -14,7 +14,17 @@
     NSString *itemToReturn;
 
     /* WORK HERE */
-    
+    if (dollars < 5) {
+        itemToReturn = @"get out of my store";
+    }else if(dollars < 6){
+        itemToReturn = @"have some gum";
+    }else if (dollars < 7){
+        itemToReturn = @"have an apple";
+    }else if (dollars < 10001){
+        itemToReturn = @"have an Apple computer";
+    }else{
+        itemToReturn = @"have The Big Apple";
+    }
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
