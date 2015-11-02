9bdbf4a13f1cc16c3d12764549379a09ec166915
Jin Kato <jin.kato@starcomww.com>
2015-11-02 17:56:27 -0500

    chap 16 complete

--- BlocExercises/BlocExercises/StuffRememberer.h
+++ BlocExercises/BlocExercises/StuffRememberer.h
@@ -9,7 +9,9 @@
 #import <Foundation/Foundation.h>
 
 @interface StuffRememberer : NSObject
-
+@property NSMutableArray *memoryOfArray;
+@property (nonatomic, copy) NSMutableArray *storedArray;
+@property (assign) CGFloat memoryOfFloat;
 /*
  
  In this class, there are three pairs of methods.
